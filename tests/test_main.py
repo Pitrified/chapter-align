85bac4dbbf6b28eaa19a3f12da7af137e7843916
Pitrified <nobilipietro@gmail.com>
2021-03-08 23:32:24 +0100

    Add test for CLI with args

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,37 @@ def runner():
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
+def test_main_succeeds_default_args(runner: CliRunner) -> None:
+    """It exits with a status code of zero, using the default args."""
     result = runner.invoke(main)
+    assert result.exit_code == 0
+
+
+def test_main_succeeds_with_args(runner: CliRunner) -> None:
+    """It exits with a status code of zero, using CLI args."""
+    result = runner.invoke(
+        main,
+        [
+            "--book_base_folder",
+            "data/leroux/yellow_room",
+            "--language0",
+            "english",
+            "--language1",
+            "french",
+            "--ch_template0",
+            "ch_{:04d}.xhtml",
+            "--ch_template1",
+            "main{}.xml",
+            "--ch_start_index0",
+            "1",
+            "--ch_start_index1",
+            "0",
+            "--tot_chapter_num",
+            "10",
+            "--author_name",
+            "Author",
+            "--book_title",
+            "Title",
+        ],
+    )
     assert result.exit_code == 0
